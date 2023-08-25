e5688ec6cd0018ec69fc90e6720f880a9f013a83
arielbatoon09 <info.arielbatoon@gmail.com>
2023-08-25 13:12:58 +0800

    Added TailwindCSS

--- src/pages/auth/signup.vue
+++ src/pages/auth/signup.vue
@@ -1,10 +1,12 @@
 <script setup>
-
 </script>
 
 <template>
     <f7-page name="signup">
         <f7-nav-left back-link="Back"></f7-nav-left>
-        
+
+        <p class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </p>
     </f7-page>
 </template>
